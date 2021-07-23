f42350165aab4789d25bb7551c578da9c0399f27
spectazular <33219668+spectazular@users.noreply.github.com>
2021-07-23 16:47:38 +1200

    updated bash script for lambda api url

--- WelcomeMessage.API/Bash Scripts/SSM APU Url Script.sh
+++ WelcomeMessage.API/Bash Scripts/SSM APU Url Script.sh
@@ -1,5 +1,5 @@
-﻿AWS_REGION=ap-southeast-2
+﻿AWS_REGION=<region>
 API_GATEWAY_STAGE=Prod
 API_GATEWAY_ID=$(aws apigateway get-rest-apis | jq '.items | .[] | select(.name == "<lambda application name>") | .id' --raw-output)
-echo "https://$API_GATEWAY_ID.execute-api.$AWS_REGION.amazonaws.com/$API_GATEWAY_STAGE"
-aws ssm put-parameter --name "/dotnetonaws/welcomemessageapi/url" --value "http://$CLUSTER_ALB_URL" --type "SecureString""
+APU_GATEWAY_URL="https://$API_GATEWAY_ID.execute-api.$AWS_REGION.amazonaws.com/$API_GATEWAY_STAGE"
+aws ssm put-parameter --name "/dotnetonaws/welcomemessageapi/url" --value "$APU_GATEWAY_URL/api" --type "SecureString"
