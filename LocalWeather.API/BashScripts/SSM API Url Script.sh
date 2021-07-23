8647bff8885e6ce6b6d42a0e166cddcbe0239d9b
spectazular <33219668+spectazular@users.noreply.github.com>
2021-07-23 16:03:33 +1200

    added bash script to generate parameter store urls to store API endoint urls

---
+++ LocalWeather.API/BashScripts/SSM API Url Script.sh
@@ -0,0 +1,11 @@
+﻿CLUSTER_ARN=$(aws ecs list-clusters | jq '.clusterArns | map(select(. | contains("/<ecs cluster name>")))[0]' --raw-output)
+echo $CLUSTER_ARN
+CLUSTER_SERVICE_ARN=$(aws ecs list-services --cluster $CLUSTER_ARN | jq '.serviceArns[0]' --raw-output)
+echo $CLUSTER_SERVICE_ARN
+CLUSTER_ALB_TG_ARN=$(aws ecs describe-services --cluster $CLUSTER_ARN --services $CLUSTER_SERVICE_ARN | jq '.services | .[] | .loadBalancers[0].targetGroupArn' --raw-output)
+echo $CLUSTER_ALB_TG_ARN
+CLUSTER_ALB_ARN=$(aws elbv2 describe-target-groups --target-group-arns $CLUSTER_ALB_TG_ARN | jq '.TargetGroups[0].LoadBalancerArns[0]' --raw-output)
+echo $CLUSTER_ALB_ARN
+CLUSTER_ALB_URL=$(aws elbv2 describe-load-balancers --load-balancer-arns $CLUSTER_ALB_ARN | jq '.LoadBalancers | .[] | .DNSName' --raw-output)
+echo $CLUSTER_ALB_URL
+aws ssm put-parameter --name "/dotnetonaws/localweatherapi/url" --value "http://$CLUSTER_ALB_URL" --type "SecureString"
---
+++ WelcomeMessage.API/Bash Scripts/SSM APU Url Script.sh
@@ -0,0 +1,5 @@
+﻿AWS_REGION=ap-southeast-2
+API_GATEWAY_STAGE=Prod
+API_GATEWAY_ID=$(aws apigateway get-rest-apis | jq '.items | .[] | select(.name == "<lambda application name>") | .id' --raw-output)
+echo "https://$API_GATEWAY_ID.execute-api.$AWS_REGION.amazonaws.com/$API_GATEWAY_STAGE"
+aws ssm put-parameter --name "/dotnetonaws/welcomemessageapi/url" --value "http://$CLUSTER_ALB_URL" --type "SecureString""
