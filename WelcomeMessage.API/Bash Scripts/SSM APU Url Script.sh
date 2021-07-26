dab697653fccd64a5c279e4119569b21a2261ba4
spectazular <33219668+spectazular@users.noreply.github.com>
2021-07-26 16:07:43 +1200

    Updated ssm script

--- WelcomeMessage.API/Bash Scripts/SSM APU Url Script.sh
+++ WelcomeMessage.API/Bash Scripts/SSM APU Url Script.sh
@@ -1,5 +1,4 @@
-﻿AWS_REGION=<region>
-API_GATEWAY_STAGE=Prod
-API_GATEWAY_ID=$(aws apigateway get-rest-apis | jq '.items | .[] | select(.name == "<lambda application name>") | .id' --raw-output)
-APU_GATEWAY_URL="https://$API_GATEWAY_ID.execute-api.$AWS_REGION.amazonaws.com/$API_GATEWAY_STAGE"
+﻿API_GATEWAY_STAGE=Prod
+API_GATEWAY_ID=$(aws apigateway get-rest-apis | jq '.items | .[] | select(.name == "WelcomeMessageAPI") | .id' --raw-output)
+APU_GATEWAY_URL="https://$API_GATEWAY_ID.execute-api.$AWS_DEFAULT_REGION.amazonaws.com/$API_GATEWAY_STAGE"
 aws ssm put-parameter --overwrite --name "/dotnetonaws/welcomemessageapi/url" --value "$APU_GATEWAY_URL/api" --type "String"
