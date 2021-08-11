eae0e7f1392667c2191651da2f532949f22c94f9
spectazular <33219668+spectazular@users.noreply.github.com>
2021-08-11 21:07:47 +1200

    deleted unused scripts

--- src/WelcomeMessage.API/Bash Scripts/SSM APU Url Script.sh
+++
@@ -1,4 +0,0 @@
-﻿API_GATEWAY_STAGE=Prod
-API_GATEWAY_ID=$(aws apigateway get-rest-apis | jq '.items | .[] | select(.name == "WelcomeMessageAPI") | .id' --raw-output)
-APU_GATEWAY_URL="https://$API_GATEWAY_ID.execute-api.$AWS_DEFAULT_REGION.amazonaws.com/$API_GATEWAY_STAGE"
-aws ssm put-parameter --overwrite --name "/dotnetonaws/welcomemessageapi/url" --value "$APU_GATEWAY_URL/api" --type "String"
